ac72717ce52b7206929e9529e4f6eb7e93d7667f
Zakhar Pochuvalov <94977621+sobselena@users.noreply.github.com>
2026-04-09 18:24:04 +0300

    feat: add basic styles for main page components

--- src/pages/main/main-page.tsx
+++ src/pages/main/main-page.tsx
@@ -1,7 +1,25 @@
 import { useGetCatsImagesQuery } from '../../redux/api';
+import { Card } from './components/card';
+import styles from './main-page.module.scss';
 
 export const MainPage = () => {
-  const { data } = useGetCatsImagesQuery();
-  console.log(data);
-  return <section>Main Page</section>;
+  const { data: cats = [], isFetching, isError } = useGetCatsImagesQuery();
+
+  return (
+    <section className={styles.container}>
+      {isError && <p className={styles.statusError}>Ошибка при загрузке котиков :(</p>}
+
+      {!isFetching && !isError && cats.length === 0 && (
+        <p className={styles.status}>Котики не найдены</p>
+      )}
+
+      <div className={styles.grid}>
+        {cats.map((cat) => (
+          <Card key={cat.id} url={cat.url} />
+        ))}
+      </div>
+
+      {isFetching && <p className={styles.loader}>... загружаем еще котиков ...</p>}
+    </section>
+  );
 };
